bea4546430beeb77af68aa83678b7b1295e25210
vvcruz <viniciusvcruz@gmail.com>
2015-09-25 16:41:36 +0200

    self-abs debug

--- run_raman_water-triolith.sh
+++ run_raman_water-triolith.sh
@@ -1085,7 +1085,9 @@ if [ "$runtype" == "-self" ]; then
 #-----------#
 #
 # I need a loop on all detuning files!!!!
-    all_detunings_files=`ls ${jobid}_*.spec | cat | cut  -f2 -d"_" | sed "s/.spec//" | awk '{printf $1" "}'`
+#    try to use the Eloss files
+#    all_detunings_files=`ls ${jobid}_*.spec | cat | cut  -f2 -d"_" | sed "s/.spec//" | awk '{printf $1" "}'`
+    all_detunings_files=`grep -i -w detun ${jobid}.log | sed "s/\<$work\>//g"`
 
     for detun in  `echo $all_detunings_files`
     do
